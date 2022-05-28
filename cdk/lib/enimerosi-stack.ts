c082d5e617ade984140d225c1368b20762e1c2ea
Niko Matsakis <niko@alum.mit.edu>
2022-05-27 21:51:36 -0400

    instead of publishing to SNS, invoke lambda
    this makes the types easier to declare, sns is annoying
    because it puts the message into an opaque string

--- cdk/lib/enimerosi-stack.ts
+++ cdk/lib/enimerosi-stack.ts
@@ -1,6 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sns_subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as ses from 'aws-cdk-lib/aws-ses';
 import * as ses_actions from 'aws-cdk-lib/aws-ses-actions';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
@@ -11,8 +13,11 @@ export class EnimerosiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'Bucket');
+    const emailsBucket = new s3.Bucket(this, 'EmailsBucket');
+    // const emailsTopic = new sns.Topic(this, 'EmailsTopic');
 
+    // Each message that is delivered will be stored into S3 and then
+    // a notification is sent to `emailsTopic`. This lambda is subscribed.
     const fn = new NodejsFunction(this, 'ProcessEmail', {
       runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'main',
@@ -23,6 +28,7 @@ export class EnimerosiStack extends Stack {
       },
       tracing: lambda.Tracing.ACTIVE,
     });
+    // emailsTopic.addSubscription(new sns_subscriptions.LambdaSubscription(fn));
 
     // NB: Because of https://github.com/aws/aws-cdk/issues/10321,
     // you must manually set the rule to active. I am not smart
@@ -34,10 +40,15 @@ export class EnimerosiStack extends Stack {
       rules: [
         {
           actions: [
+            new ses_actions.S3({
+              bucket: emailsBucket,
+              objectKeyPrefix: 'emails/',
+              // topic: emailsTopic,
+            }),
             new ses_actions.Lambda({
               function: fn,
               invocationType: ses_actions.LambdaInvocationType.EVENT,
-            }),
+            })
           ],
         }
       ],
