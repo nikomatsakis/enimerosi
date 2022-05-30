7c8c465594d41c2a6118cc1e5aec04f19ba2bde2
Niko Matsakis <niko@alum.mit.edu>
2022-05-30 05:40:17 -0400

    experimental rust version

--- cdk/lib/enimerosi-stack.ts
+++ cdk/lib/enimerosi-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as sns_subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as ses from 'aws-cdk-lib/aws-ses';
@@ -8,31 +9,24 @@ import * as ses_actions from 'aws-cdk-lib/aws-ses-actions';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RustFunction, Settings } from 'rust.aws-cdk-lambda';
 
 export class EnimerosiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    Settings.WORKSPACE_DIR = path.join(__dirname, '/..');
 
     // This bucket stores the full text of every email.
     const emailsBucket = new s3.Bucket(this, 'EmailsBucket');
 
     // Each message that is delivered will be stored into S3 and then
     // a notification is sent to `emailsTopic`. This lambda is subscribed.
-    const fn = new NodejsFunction(this, 'ProcessEmail', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: 'main',
-      environment: {
-        region: Stack.of(this).region,
-        emailsBucket: emailsBucket.bucketName,
-      },
-      entry: path.join(__dirname, `/../process-email-lambda/index.ts`),
-      bundling: {
-        minify: true,
-        externalModules: ['aws-sdk'],
-      },
-      tracing: lambda.Tracing.ACTIVE,
+    const fn = new RustFunction(this, 'ProcessEmail', {
+      directory: path.join(__dirname, `/../process-email-lambda-rs`),
     });
     emailsBucket.grantRead(fn);
+    emailsBucket.addObjectCreatedNotification(new s3n.LambdaDestination(fn));
 
     // Simple Email Service that receives notifications from github,
     // stores them in S3, and then invokes the lambda above.
@@ -51,10 +45,6 @@ export class EnimerosiStack extends Stack {
               bucket: emailsBucket,
               objectKeyPrefix: '',
             }),
-            new ses_actions.Lambda({
-              function: fn,
-              invocationType: ses_actions.LambdaInvocationType.EVENT,
-            })
           ],
         }
       ],
